e279f7cc86b60518a3816f206be869ed94bb6166
danielmpaez <53493045+danielmpaez@users.noreply.github.com>
2021-01-12 17:54:14 -0300

    Update data2.py

--- data2.py
+++ data2.py
@@ -7,13 +7,33 @@ import csv
 import json 
 from pymongo import MongoClient
 
+from flask import Flask
+from flask import request,render_template
+
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+#from app import app, mongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import jsonify, request
+#from werkzeug import generate_password_hash, check_password_hash
+
+import texttable as tt
+
+
+
 
 #LECTURA DOCUMENTO CSV
 covid19 = pd.read_csv('covid.csv')
 
+#CSV A DICCIONARIO DE PYTHON
 covid19.to_dict('records')
 covid19.head()
-print(covid19)
+
 print("------------------------------------------")
 print("------------------------------------------")
 print("FUENTE: 'PORTAL EUROPEO DE DATOS' ")
@@ -35,6 +55,60 @@ print('\n')
 print("------------------------------------------")
 Argentina =covid19[covid19['countriesAndTerritories']=='Argentina']
 print(Argentina)
+
+      
+print('\n'* 3)
+      
+print('\n'* 3)
+
+print("print de arg_casos")
+#crear archivo argentina.json,previo archivo csv
+Argentina.to_csv (r'dataframe.csv', index = False, header=True)
+#with open(Argentina.json, 'w', encoding='utf-8') as jsonf: 
+#		jsonf.write(json.dumps(data, indent=4))
+
+arg_casos = pd.read_csv('dataframe.csv')
+
+#CSV A DICCIONARIO DE PYTHON
+arg_casos.to_dict('records')
+arg_casos.head()
+        
+print(arg_casos)  
+
+
+
+
+#convertir csv a json
+
+def csv_json(csvFilePath1, jsonFilePath1): 
+	
+	# CREAMOS DICCIONARIO
+	data = {} 
+	
+	# ABRIMOS Y LEEMOS CSV
+	with open(csvFilePath1, encoding='utf-8') as csvf: 
+		csvReader = csv.DictReader(csvf) 
+		
+		
+		for rows in csvReader: 
+			
+		 
+			key = rows['deaths'] 
+			data[key] = rows 
+
+
+	with open(jsonFilePath1, 'w', encoding='utf-8') as jsonf: 
+		jsonf.write(json.dumps(data, indent=4)) 
+		
+
+csvFilePath1 = r'dataframe.csv'
+jsonFilePath1 = r'argen_data.json'
+
+csv_json(csvFilePath1, jsonFilePath1)
+
+      
+print('\n'* 3)
+      
 print('\n'* 3)
 info=Argentina[['year','month','day','cases','deaths']]
 print(info)
@@ -42,14 +116,8 @@ print('\n'* 3)
 treinta= info[:30]
 print(treinta)
 print('\n'* 3)
-#UNFO FALLECIDOS EN ARGENTINA
-print("FALLECIDOS EN ARGENTINA")
-FALLECIDOS= Argentina.loc[:'deaths']
-print(FALLECIDOS)
-#ax = FALLECIDOS.plot.barh(x="deaths", y="day")
-#FALLECIDOS.plot.hist(x="day", y="cases",ax=ax, color="C2")
-#plt.show()
 
+#DIAS CON MAS DE 150 FALLECIODS EN ARGENTINA 
 print('\n')
 print("------------------------------------------")
 print("DIAS CON MAS DE 150 FALLECIDOS")
@@ -60,7 +128,7 @@ print(promedio)
 
 
 print("------------------------------------------")
-print("TRES PRIMEROS")
+print("ULTIMO MES")
 print('\n')
 print("------------------------------------------")
 
@@ -69,6 +137,9 @@ tres = Argentina[:30]
 print(tres)
 print('\n'* 3)
 tres.plot(x="day", y="cases", color="#f44265", lw=3)
+
+#uso de tabla
+
 
 
 plt.xlabel("CASOS", 
@@ -93,7 +164,7 @@ plt.title("CONTAGIADOS ULTIMOS 30 DIAS EN ARGENTINA ",
                     })
 tres.plot(x="day", y="deaths", color="#f44265", lw=3)
 plt.text(15, 4500, "Hecho en Python y Pandas",fontsize=6,color='r',rotation=270)
-
+plt.savefig('casos2.jpg')
 #graf = tres.plot(x="day", z="cases")
 
 
@@ -119,7 +190,7 @@ plt.title("FALLECIDOS ULTIMOS 30 DIAS EN ARGENTINA ",
                     'size': 10,
                     })
 plt.text(20, 60, "Hecho en Python y Pandas",fontsize=6,color='r',rotation=270)
-
+plt.savefig('fallecidos2.jpg')
 
 
 
@@ -147,12 +218,7 @@ print(minimos)
 
 print('\n')
 print('\n')
-print("------------------------------------------")
-print("DIAS CON MENOS DE 100 FALLECIDOS")
-print("------------------------------------------")
-print('\n')
-promedio2 = Argentina.loc[Argentina.deaths<500]
-print(promedio2)
+
 '''
 ax = promedio2.plot.barh(x="day", y="deaths")
 plt.show()
@@ -194,7 +260,7 @@ make_json(csvFilePath, jsonFilePath)
 
 
   
-  
+ 
   
 # c¿CONEXION A LA BASE DE MONGO DB 
 myclient = MongoClient("mongodb://localhost:27017/")  
@@ -206,7 +272,7 @@ db = myclient["covid"]
  
 Collection = db["data"] 
   
-# LCARGA Y LECTURA DE JSON
+# CARGA Y LECTURA DE JSON
 with open('covid.json') as file: 
     file_data = json.load(file) 
       
@@ -215,8 +281,112 @@ if isinstance(file_data, list):
     Collection.insert_many(file_data)   
 else: 
     Collection.insert_one(file_data) 
+    
+    #------------------------------------------
+    
+#CREACION DE LA COLECCION ARGENTINA
 
-''' COLORES 
+db = myclient["covid"] 
+   
+#SELECCION DE COLECCON
+ 
+Collection = db["argentina"] 
+
+# CARGA Y LECTURA DE JSON
+with open('argen_data.json') as file: 
+    file_data = json.load(file) 
+      
+#INSERTAMOS JSON EN DB
+if isinstance(file_data, list): 
+    Collection.insert_many(file_data)   
+else: 
+    Collection.insert_one(file_data) 
+
+
+
+
+
+
+#CRUD
+
+
+
+
+
+
+#SERVIDOR  FLASK
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+         return  render_template('index.html')
+
+
+@app.route('/postjson', methods = ['POST'])
+def postJsonHandler():
+    print ('Getting RAW Data')
+    print (request.get_data())
+    print ('Validate JSON Format')
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
+
+    
+@app.route('/index')
+def index():
+    return  render_template('index.html')
+
+@app.route('/prueba')
+def prueba():
+    return render_template('index.html', tables=[tres.to_html(classes='data', header="true")]) 
+
+
+@app.route('/tabla')
+def tabla():
+    return render_template('tabla.html', tables=[Argentina.to_html(classes='data', header="true")])
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/paises', methods=['GET'])
+def paises():
+   with open("dataframe.json") as f:
+    data3 = json.load(f)
+    return jsonify(data3)
+
+print(data3)
+   
+     
+           
+            
+		
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+
+
+if __name__ == "__main__":    
+    
+    app.run(debug=True) 
+
+
+
+'''
+COLORES 
 Alias	Color
 b	Azul
 g	verde
